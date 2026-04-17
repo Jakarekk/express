baee6a2d7a34799ba81678004eab7073680d8e2f
Jakarekk <155654529+Jakarekk@users.noreply.github.com>
2026-04-17 11:01:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('./index.js'); 
 const app = express();
-app.get('/', (req, res) => res.send('Serwer CI/CD działa!'));
+app.get('/', (req, res) => res.send('Serwer CI/CD działa na bazie forka Express!'));
 app.listen(3000, () => console.log('App listening on port 3000'));
